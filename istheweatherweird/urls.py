b66d1fb47b6a87a0e932512e21a8e11528899e6b
Jonah Bloch-Johnson <me@jonah.org>
2021-01-18 15:08:22 -0500

    trying to reset as root

--- istheweatherweird/urls.py
+++ istheweatherweird/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('itww/', include('itww.urls')),
-    url(r'^admin/', admin.site.urls),
+    # url(r'^itww/', include('itww.urls')),
+    url(r'^/', include('itww.urls'))
 ]
